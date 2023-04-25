2d6e40c1c6120a85047e3c561aeebd54eb1ee2bb
RusticMelody <101892930+RusticMelody@users.noreply.github.com>
2023-04-25 11:14:20 -0400

    Timer

---
+++ Dogs/Assets/scripts/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextMeshProUGUI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 60f;
+    public Text timerText;
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+        else
+        {
+            timeRemaining = 0;
+            Debug.Log("Time's up!");
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
